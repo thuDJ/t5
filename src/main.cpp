0a532ba695ddd1db33a19ee9ca9180d221080e47
thuDJ <2043965149@qq.com>
2020-06-29 16:10:15 +0800

    添加xtensor、Eigen调用

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <Dense>
+#include "xtensor/xarray.hpp"
+#include "xtensor/xio.hpp"
+#include "xtensor/xview.hpp"
+
 using Eigen::MatrixXd;
 int main()
 {
@@ -9,5 +13,15 @@ m(1,0) = 2.5;
 m(0,1) = -1;
 m(1,1) = m(1,0) + m(0,1);
 std::cout << m << std::endl;
+
+xt::xarray<double> arr1
+      {{1.0, 2.0, 3.0},
+       {2.0, 5.0, 7.0},
+       {2.0, 5.0, 7.0}};
+xt::xarray<double> arr2
+      {5.0, 6.0, 7.0};
+xt::xarray<double> res = xt::view(arr1, 1) + arr2;
+std::cout << res << std::endl;
+return 0;
 }
 
